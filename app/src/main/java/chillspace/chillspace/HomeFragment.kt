deb69d97fc04d4abd44c83b433a343a1cedad362
codekaust <kaustubh1303@gmail.com>
2018-12-13 15:16:11 +0530

    Sign In and Sign Up setup.

--- app/src/main/java/chillspace/chillspace/HomeFragment.kt
+++ app/src/main/java/chillspace/chillspace/HomeFragment.kt
@@ -1,16 +1,13 @@
 package chillspace.chillspace
 
-
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -18,6 +15,14 @@ class HomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        testing.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+        }
     }
 
 }
--- app/src/main/java/chillspace/chillspace/MainActivity.kt
+++ app/src/main/java/chillspace/chillspace/MainActivity.kt
@@ -2,6 +2,7 @@ package chillspace.chillspace
 
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
