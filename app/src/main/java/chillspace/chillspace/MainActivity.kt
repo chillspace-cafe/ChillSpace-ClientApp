4bf6f6264c030b8a5ee19e7fac939c5e0a878b28
codekaust <kaustubh1303@gmail.com>
2018-12-18 20:07:34 +0530

    user data access from MainActivity

--- app/src/main/java/chillspace/chillspace/MainActivity.kt
+++ app/src/main/java/chillspace/chillspace/MainActivity.kt
@@ -4,13 +4,20 @@ package chillspace.chillspace
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import chillspace.chillspace.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var user: User? = User("", "")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         //changing fragments when firebase auth changed
         firebaseAuth.addAuthStateListener {
             if (firebaseAuth.currentUser == null) {
-                when{
+                when {
                     navController.currentDestination?.id == R.id.dest_home -> navController.navigate(R.id.action_dest_home_to_dest_signin)
                     navController.currentDestination?.id == R.id.dest_email_verification -> navController.navigate(R.id.action_dest_email_verification_to_dest_signin)
                     navController.currentDestination?.id == R.id.dest_profile -> navController.navigate(R.id.action_dest_profile_to_dest_signin)
@@ -33,6 +40,19 @@ class MainActivity : AppCompatActivity() {
                 //checking if current user has his email verified
                 if (firebaseAuth.currentUser?.isEmailVerified!!) {
                     navController.navigate(R.id.dest_home)
+
+                    //getting current user data and saving as companion object
+                    FirebaseDatabase.getInstance().reference.child("User").child(firebaseAuth.currentUser?.uid.toString())
+                            .addValueEventListener(object : ValueEventListener {
+                                override fun onCancelled(p0: DatabaseError) {
+                                    Toast.makeText(this@MainActivity, "Cant find user data.", Toast.LENGTH_SHORT).show()
+                                }
+
+                                override fun onDataChange(p0: DataSnapshot) {
+                                    user = p0.getValue(User::class.java)!!
+                                }
+
+                            })
                 } else {
                     navController.navigate(R.id.dest_email_verification)
                 }
