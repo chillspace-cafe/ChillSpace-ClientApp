a34d301b2fac4524547c38dead3e38cf453549a4
codekaust <kaustubh1303@gmail.com>
2018-12-29 14:01:38 +0530

    added livedata and viewmodel for otp generation

--- app/src/main/java/chillspace/chillspace/models/GeneratedOTP.kt
+++ app/src/main/java/chillspace/chillspace/models/GeneratedOTP.kt
@@ -1,3 +1,6 @@
 package chillspace.chillspace.models
 
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 data class GeneratedOTP(var uid : String? = null, var isRunning : Boolean? = null)
