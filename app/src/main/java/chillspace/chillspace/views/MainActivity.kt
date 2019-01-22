fe6fa2779bec0e52cefa09f276d449e5fbdf1bd4
codekaust <kaustubh1303@gmail.com>
2019-01-22 12:25:57 +0530

    logo with name update

--- app/src/main/java/chillspace/chillspace/views/MainActivity.kt
+++ app/src/main/java/chillspace/chillspace/views/MainActivity.kt
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //for toolbar
+
+        //for toolbar // required for menu
         setSupportActionBar(toolbar)
 
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
